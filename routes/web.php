e182228ba3d478a81254e856c0cc32f07022d5bb
michael <jipmichael1@gmail.com>
2025-06-10 20:30:14 +0700

    Chore: Learning make all type screen and send data
    Posts untuk melihatkan list of Posts
    Post untuk melihat post tertentu by id atau slug

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,94 @@ Route::get('/', function () {
     return view('home', ['name' => 'Michael', 'title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'slug' => 'judul-artikel-1',
+            'author' => 'Bob Will',
+            'date'=> '10 June 2025',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum officiis, aut pariatur sunt fugit maiores
+            laborum obcaecati, libero debitis porro facilis dolor sint. Quibusdam ipsum eius incidunt est nam
+            laboriosam!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'slug' => 'judul-artikel-2',
+            'author' => 'Bopeng',
+            'date'=> '15 June 2025',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum officiis, aut pariatur sunt fugit maiores
+            laborum obcaecati, libero debitis porro facilis dolor sint. Quibusdam ipsum eius incidunt est nam
+            laboriosam!'
+        ],
+    ];
+    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
+});
+
+// Route::get('/posts/{id}', function($id) {
+//      $posts = [
+//         [
+//             'id' => 1,
+//             'title' => 'Judul Artikel 1',
+//             'slug' => 'judul-artikel-1',
+//             'author' => 'Bob Will',
+//             'date'=> '10 June 2025',
+//             'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum officiis, aut pariatur sunt fugit maiores
+//             laborum obcaecati, libero debitis porro facilis dolor sint. Quibusdam ipsum eius incidunt est nam
+//             laboriosam!'
+//         ],
+//         [
+//             'id' => 2,
+//             'title' => 'Judul Artikel 2',
+//              'slug' => 'judul-artikel-2',
+//             'author' => 'Bopeng',
+//             'date'=> '15 June 2025',
+//             'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum officiis, aut pariatur sunt fugit maiores
+//             laborum obcaecati, libero debitis porro facilis dolor sint. Quibusdam ipsum eius incidunt est nam
+//             laboriosam!'
+//         ],
+//     ];
+//     $post = Arr::first($posts, function($post) use ($id) {
+//         return $post['id'] == $id;
+//     });
+
+//     if(!$post) abort(404);
+
+//     return view('post', ['title' => 'Single Post', 'post' => $post]);
+// });
+
+Route::get('/posts/{slug}', function($slug) {
+     $posts = [
+        [
+            'id' => 1,
+            'title' => 'Judul Artikel 1',
+            'slug' => 'judul-artikel-1',
+            'author' => 'Bob Will',
+            'date'=> '10 June 2025',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum officiis, aut pariatur sunt fugit maiores
+            laborum obcaecati, libero debitis porro facilis dolor sint. Quibusdam ipsum eius incidunt est nam
+            laboriosam!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+             'slug' => 'judul-artikel-2',
+            'author' => 'Bopeng',
+            'date'=> '15 June 2025',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum officiis, aut pariatur sunt fugit maiores
+            laborum obcaecati, libero debitis porro facilis dolor sint. Quibusdam ipsum eius incidunt est nam
+            laboriosam!'
+        ],
+    ];
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if(!$post) abort(404);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
@@ -15,5 +101,5 @@ Route::get('/about', function () {
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
